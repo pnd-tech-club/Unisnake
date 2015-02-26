b430a154a785969456e2556f0235849592d3a12b
matthewknecht <1800887@matthewknecht.local>
2015-02-26 14:24:38 -0600

    Revival/somewhat possible basic program outlining(I have no idea how to really start this xD)

--- main.java
+++ main.java
@@ -1,1 +1,10 @@
 /* stuff */
+public class main
+{
+	public static void main( String[] args )
+	{
+		String idek;
+		idek = "Woah! Matthew kinda knows Java?!?! What is this? But really, someone who knows what they are doing needs to start on this soon.";
+		System.out.println( idek );
+	}
+}
